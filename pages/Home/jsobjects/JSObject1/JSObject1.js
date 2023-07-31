a6810ae9b0325f7d4a6bb0f1b26d7808af306cee
Akhil Varma <akhil@appsmith.com>
2023-07-31 09:35:25 +0000

    Some Commit

---
+++ pages/Home/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	},
+	myFun3 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	}
+}
